030c199d3643b8e924c443cd0a9c8a0d699f2673
thomaschiari <thomaschiari@gmail.com>
2024-05-23 21:11:20 -0300

    Add circuit breaker for resilience4j

--- src/main/java/com/insper/store/product/ProductService.java
+++ src/main/java/com/insper/store/product/ProductService.java
@@ -5,6 +5,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +17,7 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackCreate")
     @CachePut(value = "products", key = "#result.id")
     public ProductModel create(ProductIn in) {
         return repository.save(
@@ -27,12 +29,22 @@ public class ProductService {
         );
     }
 
+    public ProductModel fallbackCreate(ProductIn in, Throwable t) {
+        return new ProductModel();
+    }
+
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackRead")
     @Cacheable(value = "products", key = "#id")
     public ProductModel read(Integer id) {
         return repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Product not found for this id :: " + id));
     }
 
+    public ProductModel fallbackRead(Integer id, Throwable t) {
+        return new ProductModel();
+    }
+
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackUpdate")
     @CachePut(value = "products", key = "#id")
     public ProductModel update(Integer id, ProductIn in) {
         ProductModel existingProduct = repository.findById(id)
@@ -44,11 +56,22 @@ public class ProductService {
         return repository.save(existingProduct);
     }
 
+    public ProductModel fallbackUpdate(Integer id, ProductIn in, Throwable t) {
+        return new ProductModel();
+    }
+
+
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackDelete")
     @CacheEvict(value = "products", key = "#id")
     public void delete(Integer id) {
         repository.deleteById(id);
     }
 
+    public void fallbackDelete(Integer id, Throwable t) {
+        // Do nothing
+    }
+
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackReadAll")
     @Cacheable(value = "allProducts")
     public List<ProductModel> readAll() {
         Iterable<ProductModel> allProducts = repository.findAll();
